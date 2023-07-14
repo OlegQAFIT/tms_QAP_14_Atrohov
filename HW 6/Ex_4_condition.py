79497a0414eb36cc381557945d0e995242e298ee
Oleg Atrohov <oleg/atrokhau@gmail.com>
2023-07-14 15:32:08 +0300

    added type variables.py

--- HW 6/Ex_4_condition.py
+++ HW 6/Ex_4_condition.py
@@ -1,54 +1,39 @@
 # 4 Даны два файла произвольного типа. Поменять местами их
 # содержимое. Файлы должны быть бинарного типа
 import os
+
 import pickle
 
-import q as q
-
-
-fl_1 = open('Ex_4_First.bin', 'wb')
-fl_2 = open('Ex_4_Second.bin', 'wb')
-fl_3 = open('draft.bin', 'wb')
 
 nums_5 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 nums_6 = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
 
-b_1 = bytearray(nums_5)
-b_2 = bytearray(nums_6)
+def add_content():
+    b_1 = bytearray(nums_5)
+    b_2 = bytearray(nums_6)
 
-fl_1.write(b_1)
-fl_2.write(b_2)
+    with open('Ex_4_First.bin', '+b') as file_1:
+        file_1.write(b_1)
+        dat_1 = file_1.read()
+    with open('Ex_4_Second.bin', '+b') as file_2:
+        file_2.write(b_2)
+        dat_2 = file_2.read()
+    add_content()
 
-fl_1.close()
-fl_2.close()
-fl_3.close()
 
-fl_1 = open('Ex_4_First.bin', 'rb')
-dat_1 = fl_1.read()
-print(dat_1)
-fl_1.close()
+def change_content():
+    global dat_2
+    global dat_1
+    with open('Ex_4_First.bin', 'wb') as f_1:
+        f_1.write(dat_2)
+    with open('Ex_4_Second.bin', 'wb') as f_2:
+        f_2.write(dat_1)
 
-fl_3 = open('draft.bin', 'wb')
-fl_3.write(dat_1)
-fl_3.close()
-
-fl_2 = open('Ex_4_Second.bin', 'rb')
-dat_2 = fl_2.read()
-print(dat_2)
-fl_2.close()
-
-fl_1 = open('Ex_4_First.bin', 'wb')
-fl_1.write(dat_2)
-fl_1.close()
-
-fl_2 = open('Ex_4_Second.bin', 'wb')
-fl_2.write(dat_1)
-fl_2.close()
+    change_content()
 
 
 
-# s = pickle.dump(b_1, fl_1)
-# q = pickle.dump(b_2, fl_2)
+
 
 
 

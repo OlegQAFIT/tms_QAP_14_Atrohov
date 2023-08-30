7aa8c2d81142cd4876440c1157f784303d66a706
Oleg Atrohov <oleg/atrokhau@gmail.com>
2023-08-30 10:22:26 +0300

    bug fixes homework 10

--- HW 10/flowers.py
+++ HW 10/flowers.py
@@ -1,67 +1,57 @@
-# Цветочница.
-# Определить иерархию и создать несколько цветов. Собрать букет с использованием аксессуаров с определением его стоимости.
-# Определить время его увядания по среднему времени жизни всех цветов в букете.
-# Позволить сортировку цветов стоимости
-# Узнать, есть ли цветок в букете.
+class Flower:
+    def __init__(self, name, price, lifespan):
+        self.name = name
+        self.price = price
+        self.lifespan = lifespan
 
 
-def create_flower(name, price, lifespan):
-    return {"name": name, "price": price, "lifespan": lifespan}
+class Accessory:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
 
 
-def create_accessory(name, price):
-    return {"name": name, "price": price}
+class Bouquet:
+    def __init__(self):
+        self.flowers = []
+        self.accessories = []
+
+    def add_flower(self, flower):
+        self.flowers.append(flower)
+
+    def add_accessory(self, accessory):
+        self.accessories.append(accessory)
+
+    def calculate_cost(self):
+        return sum(flower.price for flower in self.flowers) + sum(accessory.price for accessory in self.accessories)
+
+    def average_lifespan(self):
+        if not self.flowers:
+            return 0
+        return sum(flower.lifespan for flower in self.flowers) / len(self.flowers)
+
+    def sort_by_price(self):
+        self.flowers.sort(key=lambda flower: flower.price)
+
+    def is_flower_in_bouquet(self, flower_name):
+        return any(flower.name == flower_name for flower in self.flowers)
 
 
-def create_bouquet():
-    return {"flowers": [], "accessories": []}
+rose = Flower("Роза", 8, 7)
+tulip = Flower("Ромашка", 4, 5)
+ribbon = Accessory("Пион", 15)
 
+bouquet = Bouquet()
+bouquet.add_flower(rose)
+bouquet.add_flower(tulip)
+bouquet.add_accessory(ribbon)
 
-def add_flower(bouquet, flower):
-    bouquet["flowers"].append(flower)
+print(f"Общая стоимость букета: {bouquet.calculate_cost()} byn")
+print(f"Средняя продолжительность жизни цветов: {bouquet.average_lifespan()} дней")
 
+bouquet.sort_by_price()
+for flower in bouquet.flowers:
+    print(f"{flower.name}: {flower.price} byn")
 
-def add_accessory(bouquet, accessory):
-    bouquet["accessories"].append(accessory)
-
-
-def calculate_cost(bouquet):
-    flower_cost = sum(flower["price"] for flower in bouquet["flowers"])
-    accessory_cost = sum(accessory["price"] for accessory in bouquet["accessories"])
-    return flower_cost + accessory_cost
-
-
-def average_lifespan(bouquet):
-    flowers = bouquet["flowers"]
-    if not flowers:
-        return 0
-    return sum(flower["lifespan"] for flower in flowers) / len(flowers)
-
-
-def sort_by_price(bouquet):
-    bouquet["flowers"].sort(key=lambda flower: flower["price"])
-
-
-def is_flower_in_bouquet(bouquet, flower_name):
-    flowers = bouquet["flowers"]
-    return any(flower["name"] == flower_name for flower in flowers)
-
-
-rose = create_flower("Роза", 8, 7)
-tulip = create_flower("Ромашка", 4, 5)
-ribbon = create_accessory("Пион", 15)
-
-bouquet = create_bouquet()
-add_flower(bouquet, rose)
-add_flower(bouquet, tulip)
-add_accessory(bouquet, ribbon)
-
-print(f"Общая стоимость букета: {calculate_cost(bouquet)} byn")
-print(f"Средняя продолжительность жизни цветов в букете: {average_lifespan(bouquet)} дней")
-
-sort_by_price(bouquet)
-for flower in bouquet["flowers"]:
-    print(f"{flower['name']}: {flower['price']} byn")
-
-print(f"Роза в букете? {is_flower_in_bouquet(bouquet, 'Rose')}")
-print(f"Лилия в букете? {is_flower_in_bouquet(bouquet, 'Lily')}")
+print(f"Роза в букете? {bouquet.is_flower_in_bouquet('Роза')}")
+print(f"Лилия в букете? {bouquet.is_flower_in_bouquet('Лилия')}")
